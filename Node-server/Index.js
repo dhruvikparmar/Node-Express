76f96212eff54fce20a037b49770db1ae9c2ffa8
dhruvikparmar <dhruvikparmar1008@gmail.com>
2025-09-02 12:15:53 +0530

    Server 1

---
+++ Node-server/Index.js
@@ -0,0 +1,38 @@
+const http = require("http")
+const fs = require("fs")
+const port = 1090
+
+const porthandler = (req, res) => {
+    let filename = ''
+    switch (req.url) {
+        case '/':
+            filename = './Index.html'
+            break;
+        default:
+            filename = './error.html'
+            break;
+    }
+
+    fs.readFile(filename, (err, result) => {
+        if (!err) {
+            res.end(result)
+        }
+        else {
+            res.write("404 page not found")
+            res.end()
+        }
+    })
+}
+
+const server = http.createServer(porthandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log("server not found");
+
+    }
+    else {
+        console.log("server strted on port " + port);
+    }
+})
+
